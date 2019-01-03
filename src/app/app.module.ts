9b1c9146b927a89ddc933a35589aab5a7a7fbb64
{camilotabares} <{camilo.tabares.84@gmail.com}>
2019-01-03 17:17:55 -0500

    Setting Enviroment with Materialize and Jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from '@landing-components/landing-page.component';
+import { NavbarSocialComponent } from '@landing-components/navbar-social/navbar-social.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavbarSocialComponent
   ],
   imports: [
     BrowserModule,

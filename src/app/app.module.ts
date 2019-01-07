85513fa99e234bca00f55ad21cd3506502828960
{camilotabares} <{camilo.tabares.84@gmail.com}>
2019-01-07 17:29:34 -0500

    styling navBar menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from '@landing-components/landing-page.component';
 import { NavbarSocialComponent } from '@landing-components/navbar-social/navbar-social.component';
+import { NavbarMenuComponent } from './landing-page/navbar-menu/navbar-menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    NavbarSocialComponent
+    NavbarSocialComponent,
+    NavbarMenuComponent
   ],
   imports: [
     BrowserModule,

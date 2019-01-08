786c1ac4824b1cc3eecc62d04c1503db324b0f73
{camilotabares} <{camilo.tabares.84@gmail.com}>
2019-01-08 17:37:36 -0500

    setting karma testing for unit testing

--- src/app/landing-page/landing-page.component.spec.ts
+++ src/app/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LandingPageComponent } from './landing-page.component';
+import { LandingPageComponent } from '@landing-components/landing-page.component';
+import { MoviesFilterComponent } from '@landing-components/movies-filter/movies-filter.component';
+import { WeeklyBillboardsComponent } from '@landing-components/weekly-billboards/weekly-billboards.component';
+import { WeeklyPremieresComponent } from '@landing-components/weekly-premieres/weekly-premieres.component';
+import { NavbarSocialComponent } from '@landing-components/navbar-social/navbar-social.component';
+import { NavbarMenuComponent } from '@landing-components/navbar-menu/navbar-menu.component';
+import { MainSlidesComponent } from '@landing-components/main-slides/main-slides.component';
+import { FooterComponent } from '@landing-components/footer/footer.component';
+import { CarouselComponent } from '@landing-components/carousel/carousel.component';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
@@ -8,7 +16,18 @@ describe('LandingPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LandingPageComponent ]
+      declarations: [
+        LandingPageComponent,
+        CarouselComponent,
+        FooterComponent,
+        MainSlidesComponent,
+        MoviesFilterComponent,
+        NavbarMenuComponent,
+        NavbarSocialComponent,
+        WeeklyBillboardsComponent,
+        WeeklyPremieresComponent
+      ]
+
     })
     .compileComponents();
   }));
@@ -19,7 +38,7 @@ describe('LandingPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('debe crear el componente', () => {
     expect(component).toBeTruthy();
   });
 });
